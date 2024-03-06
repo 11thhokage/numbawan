c0c422d368ef8400b5c8e7b14040d72f5f45cd7e
Andrew Xu <miyajimaurukiora@gmail.com>
2024-03-06 10:14:42 +0800

    backend register form done.

--- numbawan/numbawan/settings.py
+++ numbawan/numbawan/settings.py
@@ -97,10 +97,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        'OPTIONS': {
+            'min_length': 6,
+        }
     },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
+    # {
+    #     "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    #     'OPTIONS': {
+    #         'no_common_passwords': False,
+    #     }
+    # },
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
--- numbawan/user/views.py
+++ numbawan/user/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.http import HttpResponseBadRequest
+from django.db import IntegrityError
 from django.shortcuts import render
 from .models import Profile
 from .forms import RegisterForm
@@ -24,9 +28,24 @@ def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            profile = Profile(user=user, **form.cleaned_data)
-            profile.save()
+            try:
+                user = form.save()
+                profile = Profile(user=user, **form.cleaned_data)
+                profile.save()
+                user_exists = User.objects.filter(email=user.email).exists()
+                if user_exists:
+                    messages.success("User successfully registered and saved in the database.")
+                else:
+                    messages.error("User registration failed.")
+            except IntegrityError as e:
+                if 'UNIQUE constraint failed' in str(e):
+                    messages.error(request, 'User already exists.')
+                else:
+                    messages.error(request, 'An error occureed while registering.')
+                return render(request, 'register.html', {'form': form})
+        else:
+            messages.error(request, f'Invalid input: {form.errors}')    
+        
     else:
         form = RegisterForm()
     return render(request, 'register.html', {'form': form})
