de6be1fb5cce4277190cad677c4227a1a8fb6171
Andrew Xu <miyajimaurukiora@gmail.com>
2024-04-01 12:43:12 +0800

    minor changes

--- numbawan/user/urls.py
+++ numbawan/user/urls.py
@@ -16,10 +16,10 @@ urlpatterns = [
     path('store/about/', views.about, name='about'),
 
     # ADMIN URLS
-    path('admin/', views.admin_index, name='admin_index'),
-    path('admin/acc_management/', views.admin_acc_management, name='admin_acc_management'),
-    path('admin/category/', views.admin_category, name='admin_category'),
-    path('admin/product/', views.admin_product, name='admin_product'),
-    path('admin/order/', views.admin_order, name='admin_order'),
-    path('admin/view_order/', views.admin_view_order, name='admin_view_order'),
+    path('s_admin/', views.admin_index, name='admin_index'),
+    path('s_admin/acc_management/', views.admin_acc_management, name='admin_acc_management'),
+    path('s_admin/category/', views.admin_category, name='admin_category'),
+    path('s_admin/product/', views.admin_product, name='admin_product'),
+    path('s_admin/order/', views.admin_order, name='admin_order'),
+    path('s_admin/view_order/', views.admin_view_order, name='admin_view_order'),
 ]
